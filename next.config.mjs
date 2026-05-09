7d65db06ed3a0e0ecb5105864eb802caaf1f50fc
hiskiatb <hiskiatb@users.noreply.github.com>
2026-05-09 23:19:07 +0700

    update dashboard

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,4 @@
-/** @type {import('next').Next.jsConfig} */
-const nextConfig = {
-  turbopack: {},
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
 
-export default nextConfig;
+export default nextConfig
